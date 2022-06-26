1e73f81fe33585c01d0e92055b4950b6ab79da25
layersony <layersony@gmail.com>
2022-06-26 08:20:36 +0300

    header skeleton done

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import './styles/compStyles.css'
+
+function Header(){
+  return (
+    <Container id='headerCont'>
+      <Row className='pt-2'>
+        <Col className='d-flex justify-content-center align-items-center flex-column'>
+          <h1>Maingi Blog</h1>
+          <p>Centered Location for all the Knowledge</p>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Header;

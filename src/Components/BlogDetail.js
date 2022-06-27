c10a90f714a54d06ae0d2ea86f11c7ba64777a6b
layersony <layersony@gmail.com>
2022-06-28 01:34:15 +0300

    img background in blogdetail remove

--- src/Components/BlogDetail.js
+++ src/Components/BlogDetail.js
@@ -15,14 +15,14 @@ function BlogDetail(){
   console.log(blogObject)
   return (
     <Container className='p-2'>
-      <div className='blogImage bg-success'>
+      <div className='blogImage'>
         <img src={blogObject ? blogObject.image : "https://bamusholdings.com/images/joomlart/demo/default.jpg"} alt="" width={600} />
       </div>
       <div>
         <h3 className="mt-3">{blogObject ? blogObject.title : ""}</h3>
         <div className='d-flex justify-content-left align-items-center mt-4 mb-4'>
-          <span>{blogObject ? blogObject.postedDate : ""}</span>
-          <span className="ms-3">Read time {blogObject ? blogObject.readTime : 0} mins</span>
+          <span>Posted Date: {blogObject ? blogObject.postedDate : ""}</span>
+          <span className="ms-3">Read time: - {blogObject ? blogObject.readTime : 0} mins </span>
         </div>
         <p>{blogObject ? blogObject.blogdata : 0}</p>
       </div>
